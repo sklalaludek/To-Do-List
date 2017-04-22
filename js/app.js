6bb49236f26300b215774b79b81cc3da85e8e474
sklalaludek <pattiegolis@gmail.com>
2017-04-22 23:45:50 +0200

    add files, background, first styling

--- js/app.js
+++ js/app.js
@@ -4,31 +4,30 @@ document.addEventListener("DOMContentLoaded", function() {
     var taskList = document.getElementById('taskList');
     var removeFinishedTasksButton = document.getElementById('removeFinishedTasksButton');
     var counter = 0;
-    var taskCounter = document.createElement('span');
-    taskCounter.style.cssText = 'display:block;width:100px;height:50px;margin:20px;';
-    document.body.insertBefore(taskCounter, taskList);
+    var taskCounter = document.getElementById('taskCounter');
 
 
     addTaskButton.addEventListener('click', function() {
+        this.classList.add('adding');
 
         if (validateInput(taskInput.value) === false) {
-            return;
+            return console.log('min 5 letters');
         }
- 
+
         var taskListLi = document.createElement('li');
-        var taskListH1 = document.createElement('h1');
+        var taskListH2 = document.createElement('h2');
         var buttonDelete = document.createElement('button');
         var buttonComplete = document.createElement('button');
-  
+
         taskList.appendChild(taskListLi);
-        taskListLi.appendChild(taskListH1);
+        taskListLi.appendChild(taskListH2);
         taskListLi.appendChild(buttonDelete);
         taskListLi.appendChild(buttonComplete);
 
         counter++;
-        taskCounter.innerText = 'Tasks to finish: ' + counter;
+        taskCounter.innerText = counter;
 
-        taskListH1.innerHTML = taskInput.value;
+        taskListH2.innerHTML = taskInput.value;
         buttonDelete.innerHTML = 'Delete';
         buttonComplete.innerHTML = 'Complete';
 
@@ -40,21 +39,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
     });
 
-   
     function addCompleteClass(button) {
         button.addEventListener('click', function() {
             this.parentElement.classList.toggle('done');
             return;
-        })
-    };
-  
+        });
+    }
+
     function removeListElement(buttonDelete, taskListLi) {
         buttonDelete.addEventListener('click', function() {
             taskListLi.parentElement.removeChild(taskListLi);
             counter--;
-            taskCounter.innerText = 'Tasks to finish: ' + counter;
-        })
-    };
+            taskCounter.innerText = counter;
+        });
+    }
 
 
     removeFinishedTasksButton.addEventListener('click', function() {
@@ -63,17 +61,29 @@ document.addEventListener("DOMContentLoaded", function() {
         for (var i = 0; i < allElementsWithClassDone.length; i++) {
             allElementsWithClassDone[i].parentElement.removeChild(allElementsWithClassDone[i]);
             counter--;
-            taskCounter.innerText = 'Tasks to finish: ' + counter;
+            taskCounter.innerText =  counter;
         }
     });
 
-  
+
     function validateInput(value) {
         if (value.length > 5 && value.length < 100) {
             return true;
         } else {
             return false;
         }
-    };
+    }
+
+    /*add animation*/
+    addTaskButton.addEventListener('transitionend', removeTransition);
+    function removeTransition(event){
+        if (event.propertyName == 'transform') {
+            this.classList.remove('adding');
+        } else {
+            return;
+        }
+    }
+
+
 
 });
